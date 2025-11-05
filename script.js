6adcd967d84937c598722aed57d61be30e78888a
Rhyz <mikhelrhyz@gmail.com>
2025-11-05 12:06:03 +0800

    implement image upload preview: - read selected file using FileReader - validate file type is image - show preview container and render selected image

---
+++ script.js
@@ -0,0 +1,25 @@
+// you will implement this logic
+// on file select -> read file -> preview -> show previewContainer
+
+const fileInput = document.getElementById("fileInput");
+const previewContainer = document.getElementById("previewContainer");
+const previewImage = document.getElementById("previewImg");
+
+fileInput.addEventListener("change", function () {
+  const file = fileInput.files[0];
+  if (!file || file.length === 0) return;
+
+  if (!file.type.startsWith("image/")) {
+    alert("Please select a valid image file.");
+    return;
+  }
+
+  const reader = new FileReader();
+
+  reader.onload = () => {
+    previewContainer.classList.remove("d-none");
+    previewImage.src = reader.result;
+  };
+
+  reader.readAsDataURL(file);
+});
